9487c84038fdef67d2d6ab05e750619a0f267658
Padmapriyab3188 <padmapriyab88@gmail.com>
2025-04-05 15:50:54 +0530

    Add files via upload

---
+++ week2/day1/LibraryManagement.java
@@ -0,0 +1,13 @@
+package week2.day1;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Library LM =new Library();
+		LM.addBook("JAVA");
+		LM.issueBook();
+	}
+
+}
