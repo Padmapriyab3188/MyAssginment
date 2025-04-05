efddbe7742020fd945f2f7657e7d4e565ae24fb3
Padmapriyab3188 <padmapriyab88@gmail.com>
2025-04-05 08:42:53 +0530

    Add files via upload

--- week1/day2/CheckNumberIsPositive.java
+++ week1/day2/CheckNumberIsPositive.java
@@ -5,12 +5,17 @@ public class CheckNumberIsPositive {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int variable =10;
+		int variable =-10;
 		
 		if(variable>0) {
 			System.out.println(variable);
 		}
-				
+			
+		else 
+		{
+			System.out.println("It is negative number");
+			
+		}
 
 	}
 
